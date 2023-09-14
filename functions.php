08543586a5e0795541f15db76d6ff377ce82f768
Matt Ondo <yours.truly@mattondo.com>
2023-09-13 23:38:04 -0600

    feature: disable gutenberg by template selection

--- functions.php
+++ functions.php
@@ -176,9 +176,6 @@ require get_template_directory() . '/inc/template-functions.php';
 require get_template_directory() . '/inc/customizer.php';
 
 /**
- * Load Jetpack compatibility file.
+ * Disable Gutenberg Editor
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
-
+require get_template_directory() . '/inc/disable-gutenberg.php';
